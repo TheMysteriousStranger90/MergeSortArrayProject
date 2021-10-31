0b75a9c411be338668bd940588291a7d6dfc53a2
Bohdan Harabadzhyu <bohdan.harabadzhyu@yandex.ru>
2021-10-31 12:10:41 +0200

    MergeSortArray (Ver.2)

--- MergeSortArrayProject/Program.cs
+++ MergeSortArrayProject/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MergeSortArrayClassLibrary;
+using static System.Console;
 
 namespace MergeSortArrayProject
 {
@@ -6,7 +8,23 @@ namespace MergeSortArrayProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var example1 = new int[] {1, 2, 3, 5, 4};
+            var example2 = new int[] {-5, 67, 32, 5, 1};
+            ShowConsoleResults(example1, example2);
+
+            ReadKey();
+        }
+
+        private static void FormatedWriteLine(int[] arr) => WriteLine($"Array: {{ {string.Join(",", arr)} }}");
+
+        private static void ShowConsoleResults(params int[][] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                FormatedWriteLine(arr[i]);
+                MergeSortArray.MergeMethod(ref arr[i]);
+                FormatedWriteLine(arr[i]);
+            }
         }
     }
 }
